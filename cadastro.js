58fb4e67ab3444167bebdb26c2425431ec23f61a
Matheus Camba <mhscamba@gmail.com>
2021-12-30 20:28:11 -0300

    validacao email pronta

--- cadastro.js
+++ cadastro.js
@@ -1,1 +1,14 @@
+// verificar email
+$("#inputEmail4").blur(function(){
+    console.log("funciona")
+    let email = $("#inputEmail4").val()
 
+   
+        if(email.indexOf("@") > -1){
+            console.log("está certo")
+            $("#email-Invalido").hide()
+        } else {
+            $("#email-Invalido").text("Email inválido")
+        }
+   
+})
